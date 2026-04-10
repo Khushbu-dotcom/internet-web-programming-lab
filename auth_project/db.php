fdd3ba3fa7e2e22bcea4da88a4fc1035de038302
khushbu singh <khushbusingh0113@gmail.com>
2026-04-10 17:24:08 +0530

    Update db.php

--- auth_project/db.php
+++ auth_project/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "WJ28@krhps", "auth_system");
+$conn = new mysqli("localhost", "root", "YOUR_PASSWORD", "auth_system");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
