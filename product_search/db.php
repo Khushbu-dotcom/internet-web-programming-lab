9788b0f7922c3d2dd6d71da8bd95cdcb50c40b81
khushbu singh <khushbusingh0113@gmail.com>
2026-04-10 17:25:40 +0530

    Update db.php

--- product_search/db.php
+++ product_search/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "WJ28@krhps", "product_db");
+$conn = new mysqli("localhost", "root", "YOUR_PASSWORD", "product_db");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
